9e5f22268d7f655d6bd5e5e8e2d2e3643470c4a4
Moon Ki Jung <m.k.jung@outlook.com>
2020-06-17 10:52:54 +0100

    First commit
    Initial commit of the souce code that was able to build a PyPI package.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mocaplib",
+    version="0.0.1",
+    author="Moon Ki Jung",
+    author_email="m.k.jung@outlook.com",
+    description="Library for Motion Capture data processing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkjung99/mocaplib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
