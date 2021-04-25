4b7511d95675eb8ae2e501008dbce02ae3ae93d8
Moon Ki Jung <m.k.jung@outlook.com>
2021-04-25 20:22:33 +0100

    Update btkapp.py
    Add get_fp_output() and get_fp_wrench() functions. get_fp_wrench() function uses btkGroundReactionWrenchFilter() function in BTK to extract the force, moment (at COP) and COP trajectory in the global coordinate system.  get_fp_output() function is my own implementation to calculate the resultant forces, moments, and COP in the global reference frame.

--- mocaplib/btkapp.py
+++ mocaplib/btkapp.py
@@ -27,6 +27,7 @@ __author__ = "Moon Ki Jung"
 import os
 import sys
 import numpy as np
+import re
 import btk
 #%%
 
@@ -127,7 +128,10 @@ def get_sub_dict_metadata(md, dict_parent, desc=False):
                 md_val = np.reshape(md_val, md_dims[::-1][:-1])
         else:
             if md_dim == 0:
-                md_val = md_val.item()
+                if md_val.shape[0] == 0:
+                    md_val = ''
+                else:
+                    md_val = md_val.item()
             else:
                 md_val = np.reshape(md_val, md_dims[::-1])
         if desc:
@@ -272,6 +276,178 @@ def get_fp_info(acq):
         dict_fp[i].update({'CAL_MATRIX': np.asarray(cal_matrix.T, dtype=np.float32)})
     return dict_fp
 
+def get_fp_output(acq, threshold=0.0):
+    pfe = btk.btkForcePlatformsExtractor()
+    pfe.SetInput(acq)
+    pfe.Update()
+    pfc = pfe.GetOutput()
+    if pfc.IsEmpty():
+        return None
+    # an_freq = acq.GetAnalogFrequency()
+    # an_fr_nums = acq.GetAnalogFrameNumber()
+    # an_res = acq.GetAnalogResolution()
+    # av_ratio = acq.GetNumberAnalogSamplePerFrame()
+    # first_fr_no = acq.GetFirstFrame()
+    # last_fr_no = acq.GetLastFrame()
+    # first_fr_idx = first_fr_no-1
+    # last_fr_idx = last_fr_no-1
+    # md_dict = get_dict_metadata(acq)
+    # point_unit = md_dict['POINT'].get('UNITS', None)
+    point_unit = acq.GetPointUnit()
+    point_scale = 1.0 if point_unit=='m' else 0.001
+    rgx_fp = re.compile(r'\S*(\d*[FMP]\d*[XxYyZz]\d*)')
+    fp_output = {}
+    fp_idx = 0
+    for fp in btk.Iterate(pfc):
+        fp_type = fp.GetType()
+        # force plate location info
+        fp_org_raw = np.squeeze(fp.GetOrigin())*point_scale
+        fp_z_check = -1.0 if fp_org_raw[2]>0 else 1.0
+        if fp_type == 1:
+            o_x = 0.0
+            o_y = 0.0
+            o_z = (-1.0)*fp_org_raw[2]*fp_z_check
+        elif fp_type==2 or fp_type==4:
+            o_x = (-1.0)*fp_org_raw[0]*fp_z_check
+            o_y = (-1.0)*fp_org_raw[1]*fp_z_check
+            o_z = (-1.0)*fp_org_raw[2]*fp_z_check
+        elif fp_type == 3:
+            o_x = 0.0
+            o_y = 0.0
+            o_z = (-1.0)*fp_org_raw[2]*fp_z_check
+            fp_len_a = np.abs(fp_org_raw[0])
+            fp_len_b = np.abs(fp_org_raw[1])
+        fp_corners = fp.GetCorners().T*point_scale
+        fp_cen = np.mean(fp_corners, axis=0)            
+        fp_p0 = fp_cen
+        fp_p1 = 0.5*(fp_corners[0]+fp_corners[3])
+        fp_p2 = 0.5*(fp_corners[0]+fp_corners[1])
+        # fp_p0 = fp_corners[2] #(-x, -y)
+        # fp_p1 = fp_corners[3] #(+x, -y)
+        # fp_p2 = fp_corners[1] #(-x, +y)
+        fp_v0 = fp_p1-fp_p0
+        fp_v1 = fp_p2-fp_p0
+        fp_v0_u = fp_v0/np.linalg.norm(fp_v0)
+        fp_v1_u = fp_v1/np.linalg.norm(fp_v1)
+        fp_v2 = np.cross(fp_v0_u, fp_v1_u)
+        fp_v2_u = fp_v2/np.linalg.norm(fp_v2)
+        fp_v_z = fp_v2_u
+        fp_v_x = fp_v0_u
+        fp_v_y = np.cross(fp_v_z, fp_v_x)
+        fp_rot_mat = np.column_stack([fp_v_x, fp_v_y, fp_v_z])
+        # force plate force/moment info
+        fp_data = {}
+        ch_data = {}
+        ch_scale = {}
+        # for ch in btk.Iterate(fp.GetChannels()):
+        fp_cnt_chs = fp.GetChannelNumber()
+        for ch_idx in range(fp_cnt_chs):
+            ch_name = fp.GetChannel(ch_idx).GetLabel()
+            ch = acq.GetAnalog(ch_name)
+            fm_name =  str.upper(rgx_fp.findall(ch.GetLabel())[0])
+            if fp_type==2 or fp_type==4:
+                label = re.sub(r'\d', r'', fm_name)
+            elif fp_type == 3:
+                label = ['FX12', 'FX34', 'FY14', 'FY23', 'FZ1', 'FZ2', 'FZ3', 'FZ4'][ch_idx]
+            ch_data[label] = np.squeeze(ch.GetData().GetValues())
+            ch_scale[label] = 0.001 if 'mm' in ch.GetUnit() else 1.0
+        if fp_type == 2:
+            f_raw = np.stack([ch_data['FX'], ch_data['FY'], ch_data['FZ']], axis=1)
+            m_raw = np.stack([ch_data['MX']*ch_scale['MX'], ch_data['MY']*ch_scale['MY'], ch_data['MZ']*ch_scale['MZ']], axis=1)
+        elif fp_type == 4:
+            fp_cal_mat = fp.GetCalMatrix()
+            fm_local = np.stack([ch_data['FX'], ch_data['FY'], ch_data['FZ'], ch_data['MX'], ch_data['MY'], ch_data['MZ']], axis=1)
+            fm_calib = np.dot(fp_cal_mat, fm_local.T).T
+            f_raw = fm_calib[:,0:3]
+            m_raw = np.stack([fm_calib[:,3]*ch_scale['MX'], fm_calib[:,4]*ch_scale['MY'], fm_calib[:,5]*ch_scale['MZ']], axis=1)
+        elif fp_type == 3:
+            fx12 = ch_data['FX12']*ch_scale['FX12']
+            fx34 = ch_data['FX34']*ch_scale['FX34']
+            fy14 = ch_data['FY14']*ch_scale['FY14']
+            fy23 = ch_data['FY23']*ch_scale['FY23']
+            fz1 = ch_data['FZ1']*ch_scale['FZ1']
+            fz2 = ch_data['FZ2']*ch_scale['FZ2']
+            fz3 = ch_data['FZ3']*ch_scale['FZ3']
+            fz4 = ch_data['FZ4']*ch_scale['FZ4']
+            fx = fx12+fx34
+            fy = fy14+fy23
+            fz = fz1+fz2+fz3+fz4
+            mx = fp_len_b*(fz1+fz2-fz3-fz4)
+            my = fp_len_a*(-fz1+fz2+fz3-fz4)
+            mz = fp_len_b*(-fx12+fx34)+fp_len_a*(fy14-fy23)
+            f_raw = np.stack([fx, fy, fz], axis=1)
+            m_raw = np.stack([mx, my, mz], axis=1)
+        fm_skip_mask = np.abs(f_raw[:,2])<=threshold
+        f_local = f_raw.copy()
+        m_local = m_raw.copy()
+        # filter local values by threshold
+        f_local[fm_skip_mask,:] = 0.0
+        m_local[fm_skip_mask,:] = 0.0
+        f_x = f_local[:,0]
+        f_y = f_local[:,1]
+        f_z = f_local[:,2]
+        m_x = m_local[:,0]
+        m_y = m_local[:,1]
+        m_z = m_local[:,2]
+        zero_vals = np.zeros((f_local.shape[0]), dtype=np.float32)
+        with np.errstate(invalid='ignore'):
+            cop_l_x = np.nan_to_num(np.where(fm_skip_mask, 0, (-m_y+(-o_z)*f_x)/f_z+o_x))
+            cop_l_y = np.nan_to_num(np.where(fm_skip_mask, 0, (m_x+(-o_z)*f_y)/f_z+o_y)) 
+            cop_l_z = zero_vals
+        t_z = m_z-(cop_l_x-o_x)*f_y+(cop_l_y-o_y)*f_x
+        # prepare local values
+        m_cop_local = np.stack([zero_vals, zero_vals, t_z], axis=1)
+        cop_local = np.stack([cop_l_x, cop_l_y, cop_l_z], axis=1)
+        f_surf_local = f_local
+        m_surf_local = np.cross(np.array([o_x, o_y, o_z], dtype=np.float32), f_local)+m_local
+        # convert local values to global
+        f_global = np.dot(f_local, fp_rot_mat)
+        m_global = np.dot(m_local, fp_rot_mat)
+        f_surf_global = np.dot(f_surf_local, fp_rot_mat)
+        m_surf_global = np.dot(m_surf_local, fp_rot_mat)
+        m_cop_global = np.dot(m_cop_local, fp_rot_mat)
+        cop_global = fp_cen+np.dot(cop_local, fp_rot_mat)
+        # prepare return values
+        fp_data.update({'TYPE': fp_type})
+        fp_data.update({'F_SURF_LOCAL': f_surf_local})
+        fp_data.update({'F_SURF_GLOBAL': f_surf_global})
+        fp_data.update({'M_SURF_LOCAL': m_surf_local})
+        fp_data.update({'M_SURF_GLOBAL': m_surf_global})
+        fp_data.update({'M_SENSOR_LOCAL': m_local})
+        fp_data.update({'M_SENSOR_GLOBAL': m_global})
+        fp_data.update({'M_COP_LOCAL': m_cop_local})
+        fp_data.update({'M_COP_GLOBAL': m_cop_global})
+        fp_data.update({'COP_LOCAL': cop_local})
+        fp_data.update({'COP_GLOBAL': cop_global})
+        fp_output.update({fp_idx: fp_data})
+        fp_idx += 1
+    return fp_output
+
+def get_fp_wrench(acq, threshold=0.0):
+    pfe = btk.btkForcePlatformsExtractor()
+    pfe.SetInput(acq)
+    pfe.Update()
+    pfc = pfe.GetOutput()
+    grwf = btk.btkGroundReactionWrenchFilter()
+    grwf.SetInput(pfc)
+    grwf.SetTransformToGlobalFrame(True)
+    grwf.SetThresholdState(True)
+    grwf.SetThresholdValue(threshold)
+    grwf.Update()
+    grwc = grwf.GetOutput()
+    grwc.Update()
+    wc_output = {}
+    for i in range(grwc.GetItemNumber()):
+        wc_data = {}
+        pos = grwc.GetItem(i).GetPosition().GetValues()
+        force = grwc.GetItem(i).GetForce().GetValues()
+        moment = grwc.GetItem(i).GetMoment().GetValues()
+        wc_data.update({'POS': pos})
+        wc_data.update({'FORCE': force})
+        wc_data.update({'MOMENT': moment})
+        wc_output.update({i: wc_data})
+    return wc_output
+
 def add_metadata(acq, parent_name, name, data, desc=None):
     md_root = acq.GetMetaData()
     grp_names = []
@@ -289,6 +465,10 @@ def add_metadata(acq, parent_name, name, data, desc=None):
         child_desc = desc
     md_child.SetDescription(child_desc)
     return True
+
+def get_metadata(acq, parent_name, name):
+    md = acq.GetMetaData()
+    return md.FindChild(parent_name).value().FindChild(name).value().GetInfo()
 
 def change_point_name(acq, old_name, new_name):
     pt = acq.FindPoint(old_name).value()
